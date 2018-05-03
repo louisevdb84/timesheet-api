c6c4876b4723bd8a1da174a4e8674377ce9e2dcf
Louise van den Berg <louisevdb84@gmail.com>
2018-05-03 16:09:43 +0200

    Add user model and started to implement passport

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose")
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    User            = require("./models/user")
 
 var url = process.env.TimesheetDbDatabaseURL;
 mongoose.connect(url);
+
+app.use(require("express-session")({
+    secret: "timesheet app",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.send("Hi!");
